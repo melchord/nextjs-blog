9409fac9303ee242848a980734bb1595fb672b0a
Melchor Dominguez <mdmngz411@gmail.com>
2021-01-26 18:34:44 -0500

    adding css to posts

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -4,21 +4,22 @@ import Head from 'next/head';
 
 import Date from '../../components/date';
 import Layout from '../../components/layout';
-import {getAllPostIds, getPostData} from '../../lib/posts';
+import { getAllPostIds, getPostData } from '../../lib/posts';
+import utilStyles from '../../styles/utils.module.css';
 
-export default function Post({postData}) {
+export default function Post({ postData }) {
   return (
     <Layout>
       <Head>
         <title>{postData.title}</title>
       </Head>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      <Date dateString={postData.date} />
-      <br />
-      <div dangerouslySetInnerHTML={{__html: postData.contentHtml}} />
+      <article>
+        <h1 className={utilStyles.headingX1}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
   );
 }
@@ -40,7 +41,7 @@ export async function getStaticPaths() {
   };
 }
 
-export async function getStaticProps({params}) {
+export async function getStaticProps({ params }) {
   const postData = await getPostData(params.id);
   return {
     props: {
